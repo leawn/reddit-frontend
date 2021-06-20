07efa0598dcd9057c0038257866e9a2ed4242635
Leon <leonrubner2809@gmail.com>
2021-06-20 21:03:28 +0200

    weeknd commit

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,22 +1,41 @@
 import React from "react";
 import { Formik, Form } from "formik";
-import { FormControl, FormLabel, Input, FormErrorMessage, Box, Button } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 import { Wrapper } from "../components/Wrapper"
 import { InputField } from '../components/InputField';
+import { useMutation } from "urql";
 
 interface registerProps {
 
 }
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!) {
+    register(options: { username: $username, password: $password }) {
+        errors {
+            field
+            message
+        }
+        user {
+            id
+            username
+        }
+    }
+}
+`
+
 const Register: React.FC<registerProps> = ({}) => {
-        return (
+    const [, register] = useMutation(REGISTER_MUT);
+    return (
             <Wrapper variant="regular">
                 <Formik 
                     initialValues={{ username: "", password: ""}}
-                    onSubmit={values => {
+                    onSubmit={ async (values, actions) => {
                         console.log(values);
+                        await register(values);
+                        actions.setSubmitting(false);
                     }}>
-                    {( isSubmitting ) => (
+                    {( props ) => (
                         <Form>
                             <Box>
                                 <InputField
@@ -33,7 +52,7 @@ const Register: React.FC<registerProps> = ({}) => {
                                     type="passsword"
                                 />
                             </Box>
-                            <Button mt={4} type="submit" isLoadin={isSubmitting} colorScheme="teal">register</Button>
+                            <Button mt={4} type="submit" isLoading={props.isSubmitting} colorScheme="teal">register</Button>
                         </Form>
                     )}
                 </Formik>
