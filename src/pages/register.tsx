71d7ffcbbf215a140d2ecf0f01e01008978d7417
Leon <leonrubner2809@gmail.com>
2021-06-21 12:45:34 +0200

    montag commit

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,37 +3,29 @@ import { Formik, Form } from "formik";
 import { Box, Button } from '@chakra-ui/react';
 import { Wrapper } from "../components/Wrapper"
 import { InputField } from '../components/InputField';
-import { useMutation } from "urql";
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../util/toErrorMap';
+import { useRouter } from "next/router";
 
-interface registerProps {
-
-}
-
-const REGISTER_MUT = `
-mutation Register($username: String!, $password: String!) {
-    register(options: { username: $username, password: $password }) {
-        errors {
-            field
-            message
-        }
-        user {
-            id
-            username
-        }
-    }
-}
-`
+interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
-    const [, register] = useMutation(REGISTER_MUT);
+    const router = useRouter();
+    const [, register] = useRegisterMutation();
     return (
             <Wrapper variant="regular">
                 <Formik 
                     initialValues={{ username: "", password: ""}}
-                    onSubmit={ async (values, actions) => {
+                    onSubmit={ async (values, { setSubmitting, setErrors }) => {
                         console.log(values);
-                        await register(values);
-                        actions.setSubmitting(false);
+                        const response = await register(values);
+                        if (response.data?.register.errors) {
+                            setErrors(toErrorMap(response.data.register.errors));
+                        } else if (response.data?.register.user) {
+                            // worked
+                            router.push("/");
+                        }
+                        setSubmitting(false);
                     }}>
                     {( props ) => (
                         <Form>
